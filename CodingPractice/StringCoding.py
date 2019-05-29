3814c146c3ff2268c7b926c62605fff79765a7a4
“rakeshhighpeak” <rakesh.kumar@highpeaksw.com>
2019-05-29 21:34:28 +0530

    Clean Code

--- CodingPractice/StringCoding.py
+++ CodingPractice/StringCoding.py
@@ -37,9 +37,6 @@ def forthReverseString(string):
     string = "".join(reversed(string))
     return string
 
-
-
-
 str = input("Plase enter the string    ")
 rev_str = forthReverseString(str)
 print("Original String is : ", str, "   And Reverse String is : ", rev_str)
